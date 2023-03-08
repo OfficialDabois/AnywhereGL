8ec4426d7f073e2b66b4efcace0eaf25cebbb2f6
OfficialDabois <sebastian.whitt@icloud.com>
2023-03-08 10:20:50 +0000

    More object stuff

--- Objects/3D/Cube.cpp
+++ Objects/3D/Cube.cpp
@@ -4,6 +4,14 @@
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 
-void SetCamera(glm::mat4 quCamUpdate) {
+void Cube::SetCamera(glm::mat4 quCamUpdate) {
+
+}
+
+void Cube::SetPerspective(glm::mat4 quPersUpdate) {
+
+}
+
+void Cube::Render() {
 
 }
